8ae94148116821af44f4df01357513a2a393e9ed
Thiago Pires <thiagopac@gmail.com>
2026-05-27 09:12:00 -0300

    Initial Commit

---
+++ Savory/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Savory
+//
+//  Created by Thiago Castro on 08/06/26.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Savory/SavoryApp.swift
@@ -0,0 +1,18 @@
+//
+//  SavoryApp.swift
+//  Savory
+//
+//  Created by Thiago Castro on 08/06/26.
+//
+
+
+import SwiftUI
+
+@main
+struct SavoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
